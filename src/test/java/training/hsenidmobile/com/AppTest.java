6f90bba05911c1013ad7eb2424957c79e1bffd2a
Isuru Wijesinghe <isuruw@hsenidmobile.com>
2018-10-02 20:17:26 +0530

    Fix: Initial commit

---
+++ src/test/java/training/hsenidmobile/com/AppTest.java
@@ -0,0 +1,20 @@
+package training.hsenidmobile.com;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        fail("This is Junit 5");
+    }
+}
