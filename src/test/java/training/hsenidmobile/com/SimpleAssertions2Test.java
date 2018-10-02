64473e300c0f8717325bc8fec39e230a6db65242
Isuru Wijesinghe <isuruw@hsenidmobile.com>
2018-10-02 20:50:45 +0530

    Before and After annotation samples added.

---
+++ src/test/java/training/hsenidmobile/com/SimpleAssertions2Test.java
@@ -0,0 +1,35 @@
+package training.hsenidmobile.com;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SimpleAssertions2Test {
+
+    @BeforeEach
+    public void beforeEach(TestInfo testInfo) {
+        System.out.println("Initializing test data for " + testInfo.getDisplayName());
+    }
+
+    @AfterEach
+    public void afterEach(TestInfo testInfo) {
+        System.out.println("Clean test data for " + testInfo.getDisplayName());
+    }
+
+    @Test
+    public void toUpperCase() {
+        String value = "abcd".toUpperCase();
+        assertNull(value);
+        assertEquals("ABCD", value);
+    }
+
+    @Test
+    public void containsCheck() {
+        String str = "abcd";
+        boolean isStartsWith = str.startsWith("cd");
+        assertFalse(isStartsWith);
+    }
+}
